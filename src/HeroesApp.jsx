fecad1feabd40e77f71b8d8fdb97d07fd40530ff
Antony <apalaciosc0109@gmail.com>
2024-12-23 12:06:02 -0500

    AppRouter

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,7 +1,10 @@
+import {AppRouter} from "./router/AppRouter.jsx";
+
 export const HeroesApp = () => {
     return (
         <>
             <h1>Heroes App</h1>
+            <AppRouter/>
         </>
     );
 };
