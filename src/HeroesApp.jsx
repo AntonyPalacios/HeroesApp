54f34e28a09302dda992d80ee3ea9afa0feff1f9
Antony <apalaciosc0109@gmail.com>
2024-12-23 18:39:17 -0500

    navbar y archivos de barril

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -3,7 +3,6 @@ import {AppRouter} from "./router/AppRouter.jsx";
 export const HeroesApp = () => {
     return (
         <>
-            <h1>Heroes App</h1>
             <AppRouter/>
         </>
     );
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,13 @@
 import {Navigate, Route, Routes} from "react-router-dom";
-import {MarvelPage} from "../heroes/pages/MarvelPage.jsx";
-import {DcPage} from "../heroes/pages/DcPage.jsx";
-import {LoginPage} from "../auth/pages/LoginPage.jsx";
+import {Navbar} from "../ui";
+import {DcPage, MarvelPage} from "../heroes";
+import {LoginPage} from "../auth";
+
 
 export const AppRouter = () => {
     return (
         <>
+            <Navbar/>
             <Routes>
                 <Route path="marvel" element={<MarvelPage />} />
                 <Route path="dc" element={<DcPage />} />
