33b246a00ac502be97f389e53df2c71e3f7fb736
Antony <apalaciosc0109@gmail.com>
2024-12-23 19:05:00 -0500

    refactorizando rutas: más de 1 router

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,7 +1,14 @@
-import { Link, NavLink } from 'react-router-dom';
+import {Link, NavLink, useNavigate} from 'react-router-dom';
 
 
 export const Navbar = () => {
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        navigate('/login',{
+            replace: true,
+        });
+    }
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
 
@@ -16,17 +23,23 @@ export const Navbar = () => {
                 <div className="navbar-nav">
 
                     <NavLink
-                        className={({isActive})=> `nav-item nav-link ${isActive ? 'active' : ''}`}
+                        className={({isActive}) => `nav-item nav-link ${isActive ? 'active' : ''}`}
                         to="/marvel"
                     >
                         Marvel
                     </NavLink>
 
                     <NavLink
-                        className={({isActive})=> `nav-item nav-link ${isActive ? 'active' : ''}`}
+                        className={({isActive}) => `nav-item nav-link ${isActive ? 'active' : ''}`}
                         to="/dc"
                     >
                         DC
+                    </NavLink>
+                    <NavLink
+                        className={({isActive}) => `nav-item nav-link ${isActive ? 'active' : ''}`}
+                        to="/search"
+                    >
+                        Search
                     </NavLink>
                 </div>
             </div>
@@ -42,7 +55,11 @@ export const Navbar = () => {
                     <span className="nav-item nav-link text-primary">
                         Antony
                     </span>
-                    <button className="nav-item nav-link btn">Logout</button>
+                    <button
+                        className="nav-item nav-link btn"
+                        onClick={onLogout}
+                    >Logout
+                    </button>
                 </ul>
             </div>
         </nav>
